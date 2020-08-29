0a1f1dec11e7f5459b284931dfb1140ba56804e6
Vladyslav_Shynder <shynderv@gmail.com>
2020-08-29 23:52:18 +0300

    Updates

---
+++ precache-manifest.0aabe7a9cc4d6650c843dbc57428df65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b9093c30dc624d2ed9c2768c072a55",
+    "url": "/hw-react-06-phonebook/index.html"
+  },
+  {
+    "revision": "d79ee235e1e13d5a9c57",
+    "url": "/hw-react-06-phonebook/static/css/main.cdcb6b6d.chunk.css"
+  },
+  {
+    "revision": "57daa0774d4c4606e94b",
+    "url": "/hw-react-06-phonebook/static/js/2.532679bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hw-react-06-phonebook/static/js/2.532679bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79ee235e1e13d5a9c57",
+    "url": "/hw-react-06-phonebook/static/js/main.7f4a65ba.chunk.js"
+  },
+  {
+    "revision": "5a763bb238a4e7a04770",
+    "url": "/hw-react-06-phonebook/static/js/runtime-main.1c5df59e.js"
+  }
+]);
